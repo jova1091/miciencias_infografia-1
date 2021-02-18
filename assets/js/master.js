f232a436b59a899c86396541a6ba8aefc10049a5
Joan Preciado <48997866+jova1091@users.noreply.github.com>
2021-02-18 14:38:08 -0500

    First commit
    Primer commit versión estable

---
+++ assets/js/master.js
@@ -0,0 +1,51 @@
+$(window).scroll(function () {
+
+    $('.img').each(function(){
+        var target = $(this),windowHeight = $(window).height();
+        
+        console.log(target)
+        
+        //Si el elemento es visible al realizar scroll
+        if (inView(target)) {
+            var offset = getViewportOffset(target);
+            //Si el elemento aparece por la parte de abajo de la ventana
+            if (windowHeight > offset.top) {
+                $(target).removeClass('hide').addClass('animate__zoomInDown');
+            }
+        }
+    });
+});
+
+function inView(element, fullHeightInView) {
+    var $element = $(element);
+    var $window = $(window);
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+    var elemTop = $element.offset().top;
+    var elemBottom;
+
+    if (fullHeightInView) {
+        elemBottom = elemTop + $element.height();
+    } else {
+        elemBottom = elemTop;
+    }
+    
+    return ((elemBottom >= docViewTop) &&
+        (elemTop <= docViewBottom) &&
+        (elemBottom <= docViewBottom) &&
+        (elemTop >= docViewTop));
+}
+
+function getViewportOffset(element) {
+    var $element = $(element),
+        $window = $(window);
+
+    var scrollLeft = $window.scrollLeft(),
+        scrollTop = $window.scrollTop(),
+        offset = $element.offset();
+
+    return {
+        left: offset.left - scrollLeft,
+        top: offset.top - scrollTop
+    };
+}
